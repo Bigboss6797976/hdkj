31607b45a808ee263a1e1c38382e007daab4e882
Bigboss6797976 <yge7816@gmail.com>
2026-05-12 02:48:45 +0800

    Update MockToken.sol

--- contracts/MockToken.sol
+++ contracts/MockToken.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockToken is ERC20 {
+    constructor() ERC20("TestUSDT", "TUSDT") {
+        _mint(msg.sender, 1000000 * 10**6); // 100万枚，6位小数
+    }
+    
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
+}
